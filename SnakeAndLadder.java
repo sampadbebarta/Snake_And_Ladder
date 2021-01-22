515de8f2e6482aa3404ae5f204d53cca9a6bd599
Sampad <bebartasampad@gmail.com>
2021-01-22 13:29:51 +0530

    Welcome to Snake and Ladder Game

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+public class SnakeAndLadder {
+
+        //Single player at start position 0;
+        public static int position_of_player1=0;
+
+        public static void main(String args[])
+        {
+            System.out.println("WELCOME TO SNAKE AND LADDER GAME");
+        }
+
+
+}
