c046024b0365fb2f3ef3a3cf8cf3ac6b3bb9858c
Sampad <bebartasampad@gmail.com>
2021-01-22 15:21:42 +0530

    Player reaches winning position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,28 +1,41 @@
 public class SnakeAndLadder {
-	public static final int IS_LADDER = 1;
-   public static final int IS_SNAKE = 2;
-	public static void main(String[] args) {
-	System.out.println("WELCOME TO SNAKE AND LADDER GAME");
-	int startingPos=0;
-	int currentPos=0;
-	int play;
-	int dice;
-	System.out.println("Player is Starting at: " + startingPos);
-	dice = (int) ((Math.floor(Math.random() * 10) % 6) + 1);
-   System.out.println("Current Dice Face is: " + dice);
-   play = (int) ((Math.floor(Math.random() * 10) % 3));
-   switch (play) {
-     case IS_LADDER:
-        currentPos += dice;
-        break;
-     case IS_SNAKE:
-        currentPos -= dice;
-        break;
-     default:
-        currentPos+=0;
-        break;
-    	}
-   System.out.println("New Position is: " + currentPos);
+	 public static final int IS_LADDER = 1;
+    public static final int IS_SNAKE = 2;
+	 public static void main(String[] args) {
+		 System.out.println("WELCOME TO SNAKE AND LADDER GAME");
+		 int startingPos=0;
+	    int currentPos=0;
+	    int play;
+	    int dice;
+	    System.out.println("Player is Starting at: " + startingPos);
+	    while (currentPos != 100) {
+	    	dice = (int) ((Math.floor(Math.random() * 10) % 6) + 1);
+	    	System.out.println("Current Dice Face is: " + dice);
+	    	play = (int) ((Math.floor(Math.random() * 10) % 3));
+	    	switch (play) {
+        		case IS_LADDER:
+        			currentPos += dice;
+        			break;
+        		case IS_SNAKE:
+        			currentPos -= dice;
+        			break;
+        		default:
+        			currentPos+=0;
+        			break;
+	    	}
+	    	if (currentPos == 100) {
+                break;
+            } else if (currentPos > 100) {
+                currentPos -= dice;
+                System.out.println("Its not valid and staying at same position.");
+            } else if (currentPos < 0) {
+                currentPos = startingPos; // Starting from zero again.
+                System.out.println("Back to start again");
+            } else {
+                System.out.println("New Position is: " + currentPos);
+            }
+	    }
+        System.out.println("You are Winner!!!");
 	}
 
 }
